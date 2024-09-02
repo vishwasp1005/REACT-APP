821199aa90bc1899c4ab53728c21ceea8ef53b24
VISHWAS76 <140800019+vishwasp1005@users.noreply.github.com>
2024-09-02 17:32:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// function App() {
+//   return (
+//    <h1>Hello, World!</h1>
+//   );
+// }
+
+class Car extends React.Component{
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+
+export default Car;
